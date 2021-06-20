92eacde4c758c5a9049d7ed6b68babdf2b1b1e3f
oliverharris <thejuggernaut@harriso.co.uk>
2021-06-20 22:54:21 +0100

    Link register to gateway

--- stepscoreapp/lib/assets/constants.dart
+++ stepscoreapp/lib/assets/constants.dart
@@ -1,3 +1,3 @@
 library constants;
 
-const String API_URL = "localhost:9090";
+const String API_URL = "192.168.0.114:9091";
--- stepscoreapp/lib/screens/login/auth.dart
+++ stepscoreapp/lib/screens/login/auth.dart
@@ -5,7 +5,7 @@ Future<bool> register(String username) async {
   var url = Uri.http(Constants.API_URL, '/api/auth/register');
 
   // Await the http get response, then decode the json-formatted response.
-  var response = await http.post(url, body: {'username': username});
+  var response = await http.post(url, body: username);
   if (response.statusCode == 200) {
     return true;
   } else if (response.statusCode == 409) {
